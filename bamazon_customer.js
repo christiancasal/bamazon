d4d42fef9bd82fffd8deccc4bf27952ce6c176be
ccasal <casal.christian@gmail.com>
2016-05-07 14:20:07 -0400

    fixed a bug - wasnt reducing the quantity from stock

--- bamazon_customer.js
+++ bamazon_customer.js
@@ -3,7 +3,7 @@ var prompt = require('prompt');
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: '',
     password: '',
     database: 'bamazon'
 });
@@ -13,15 +13,12 @@ var print_products = function() {
     connection.query('select * from products', function(err, res) {
         if (err) throw err;
 
-        var prompter = false
 
         res.forEach(function(row) {
           console.log(JSON.stringify(row, null, 2));
 
-          prompter = true;
+          // prompter = true;
         });
-
-        if(prompter){
 
           prompt.start();
           prompt.get(['id_to_buy', 'quantity'], function(err, res) {
@@ -31,14 +28,10 @@ var print_products = function() {
               var user_quantity = res.quantity;
 
               user_check_purchase(user_wants, user_quantity);
-              prompter = false;
               print_products();
           });
-        }
-    });
-
+      });
 }
-
 var user_check_purchase = function(id, qt) {
     connection.query('select * from products where item_id = ' + id, function(err, res) {
         if (err) throw err;
@@ -49,14 +42,14 @@ var user_check_purchase = function(id, qt) {
             console.log('insufficent stock!');
         } else {
             console.log('user may buy ' + res[0].product_name + "!")
-            update_db_w_purch(id, res[0].stock);
+            update_db_w_purch(id, res[0].stock, qt);
         }
     });
 }
-var update_db_w_purch = function(id, quantity) {
-    quantity = quantity - 1;
-    console.log(quantity);
-    connection.query('update products set stock = ' + quantity + ' where item_id = ' + id, function(err, res) {
+var update_db_w_purch = function(id,stock, qt) {
+    stock = stock - qt;
+    console.log(stock);
+    connection.query('update products set stock = ' + stock + ' where item_id = ' + id, function(err, res) {
         if (err) throw err;
         // console.log(res);
     });
--- bamazon_manager.js
+++ bamazon_manager.js
@@ -3,7 +3,7 @@ var prompt = require('prompt');
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: '',
     password: '',
     database: 'bamazon'
 });
